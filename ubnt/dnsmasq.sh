33d81d1e93c83067188dc7de0bb7c98828795eab
kingwoo <woyaya@163.com>
2024-05-26 19:56:24 +0800

    update dnsmasq config script

--- ubnt/dnsmasq.sh
+++ ubnt/dnsmasq.sh
@@ -1,9 +1,46 @@
-#!/bin/bash
-[ -d ./etc ] && BASEDIR=. || BASEDIR=""
-CONF="$BASEDIR/etc/dnsmasq.conf"
-CONF_LIST="$CONF $BASEDIR/etc/dnsmasq.d/dnsmasq-dhcp-config.conf"
-NAME=directDNS_filter
-DIRECTDNS_LIST="128/26 11 8"
+#!/bin/sh
+CONF="/etc/dnsmasq.conf"
+CONF_LIST="$CONF /etc/dnsmasq.d/dnsmasq-dhcp-config.conf"
+############################
+TEST_ONLY=${TEST_ONLY:-0}
+DEBUG=${DEBUG:-0}
+DBG_FILE=/tmp/`basename $0`_dbg.log
+rm -rf $DBG_FILE
+############################
+DBG()
+{
+	echo "$@"
+	[ "$DEBUG" = "1" ] && echo "$@" >>$DBG_FILE
+}
+############################
+USAGE(){
+	echo "Usage: $1 [params]"
+	echo "     -T: Test run, does not actually take effect"
+	echo "     -D: debug mode"
+	echo "     -h: print this"
+
+	exit -1
+}
+while getopts "TD" opt; do
+	case $opt in
+		T)
+			TEST_ONLY=1
+		;;
+		D)
+			DEBUG=1
+		;;
+		*)
+			USAGE $0
+		;;
+	esac
+done
+DBG "TEST:$TEST_ONLY, DEBUG: $DEBUG"
+############################
+[ -f ./$CONF ] && {
+	CONF=.$CONF
+	CONF_LIST=`echo $CONF_LIST | sed 's/^/ /;s/ \// .\//g'`
+}
+DBG "Config files: $CONF_LIST"
 
 subnet2range(){
 	local ip
@@ -58,7 +95,7 @@ read_conf(){
 	local value
 	for file in $CONF_LIST
 	do
-		value=`cat $file | sed "/$1/!d;s/.*$1 *//;q"`
+		value=`cat $file | sed "/\b$1/!d;s/.*\b$1 *//;q"`
 		[ -n "$value" ] && {
 			echo $value
 			return 0
@@ -69,24 +106,15 @@ read_conf(){
 
 filter_directDNS()
 {
-	# flush target chain or create it
-	iptables -t filter -F $NAME || iptables -t filter -N $NAME || return
-	# filter for DNS query
-	iptables -t filter -A FORWARD -p udp --dport 53 -j $NAME
-	# Enable ip ranges for direct-DNS device
-	for ip in $DIRECTDNS_LIST;do
-		iptables -t filter -A $NAME -s $subnet.$ip -j ACCEPT
-	done
-	# Disable direct DNS query for all other devices
-	iptables -t filter -A $NAME -j DROP
-	# Disable IPv6 direct DNS query for all devices
-	ip6tables -t filter -D FORWARD -p udp --dport 53 -j DROP 2>/dev/null
-	ip6tables -t filter -A FORWARD -p udp --dport 53 -j DROP
+	# filter for direct-DNS query
+	/sbin/iptables -t filter -D VYATTA_FW_OUT_HOOK -p udp --dport 53 -j dns_filter 2>/dev/null
+	DBG "/sbin/iptables -t filter -A VYATTA_FW_OUT_HOOK -p udp --dport 53 -j dns_filter"
+	/sbin/iptables -t filter -A VYATTA_FW_OUT_HOOK -p udp --dport 53 -j dns_filter
 }
 
-dnsmasq_set_directDNS(){
+dnsmasq_set_directDNS_line(){
 	local ip
-	ip=`echo "$1" | sed '/dhcp-host=.*,set:LAN,/!d;s/.*,\([0-9\.]\{7,15\}\).*/\1/'`
+	ip=`echo "$1" | sed '/dhcp-host=.*,set:LAN,/!d;/set:LAN,set:directdns/d;s/.*,\([0-9\.]\{7,15\}\).*/\1/'`
 	[ -z "$ip" ] && echo "$1" && return
 	ip_in_range $ip $directDNS_range || {
 		echo "$1"
@@ -94,31 +122,55 @@ dnsmasq_set_directDNS(){
 	}
 	echo "$1" | sed 's/set:LAN/set:LAN,set:directdns/'
 }
-dnsmasq_remove_port(){
-	echo "$1" | sed 's/\bport=[[:digit:]]*/port=0/'
-}
-reconfig_dnsmasq(){
+dnsmasq_set_directDNS(){
 	local file
 	local host
 	file=/tmp/`basename $1`.tmp
 	rm -rf $file
-	while read line;do
-		line=`dnsmasq_set_directDNS "$line"`
-		line=`dnsmasq_remove_port "$line"`
+	while IFS= read -r line;do
+		line=`dnsmasq_set_directDNS_line "$line"`
 
 		echo "$line" >>$file
 	done <$1
-	cat $file
-	rm $file
+	if [ "$TEST_ONLY" = "1" ]
+	then
+		cat $file
+	else
+		# check if changed
+		cmp $file $1 >/dev/null || mv $file $1
+	fi
+	[ "$DEBUG" != "1" ] && rm -rf $file
+}
+dnsmasq_disable_dns(){
+	local dnsport
+	dnsport=`read_conf "port="`
+	[ -z "$dnsport" ] && echo "port=0" >>$CONF && return
+	sed -i 's/\bport=[[:digit:]]*/port=0/' $CONF_LIST
 }
 
-# stop dnsmasq
-#systemctl stop dnsmasq
 # reconfig dnsmasq
-subnet=`read_conf "subnet " | sed 's/\.[0-9]*\/.*//'`
-directDNS_range=`subnet2range $DIRECTDNS_LIST`
+subnet=`read_conf "subnet " | sed 's/\.[0-9]\+\/.*//'`
+DBG "Subnets: $subnet"
+directdns_list=`/sbin/ipset -L directDNS 2>/dev/null | sed '1,8d' | sed "s/$subnet\.//g"`
+DBG "Direct DNS CIDR list: $directdns_list"
+[ -z "$directdns_list" ] && {
+	DBG "Can not find device/IP list from ipset(directDNS)"
+	exit 1
+}
+directDNS_range=`subnet2range $directdns_list`
+DBG "Direct DNS range: $directDNS_range"
 for file in $CONF_LIST;do
-	reconfig_dnsmasq $file
+	DBG "Processing file $file"
+	dnsmasq_set_directDNS $file
 done
-
-
+DBG "Disable DNS of dnsmasq"
+dnsmasq_disable_dns
+DBG "Setup iptables for direct DNS"
+filter_directDNS
+# restart dnsmasq
+DBG "Restart dnsmasq"
+[ "$TEST_ONLY" != "1" ] && systemctl restart dnsmasq
+# start smartDNS
+DBG "Restart smartdns"
+[ "$TEST_ONLY" != "1" ] && systemctl restart smartdns
+exit 0
